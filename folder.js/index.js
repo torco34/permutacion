92d3f3ee7bcbfab475862a438e26597547cd9cdb
torco34 <torcoromaarias@gimail.com>
2020-07-30 10:55:48 -0500

    Primer commit

---
+++ folder.js/index.js
@@ -0,0 +1,10 @@
+
+let lista = [9, 6, 8,1,3,4,]
+lista.sort(function(a, b){
+	return a - b;
+
+
+})
+ document.getElementById('pantalla').innerHTML = lista
+console.log(lista)
+console.log(`${lista}`)
